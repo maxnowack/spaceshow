cc8a06dc712a95993b4488788ecb18819ca25d56
Max Nowack <max@unsou.de>
2018-02-16 19:53:34 +0100

    Enable better controlling the process

--- src/index.js
+++ src/index.js
@@ -1,35 +1,49 @@
+import { EventEmitter } from 'events';
 import MeteorApp from './MeteorApp';
 import startPuppeteer from './startPuppeteer';
 
-export default (options) => {
-  const {
-    args,
-    appDir,
-    settings,
-    meteorPath,
-    port = 3000,
-    puppeteerOptions = {},
-  } = options || {};
+export class Spaceshow extends EventEmitter {
+  constructor(options) {
+    super();
+    this.options = options;
+    const {
+      args,
+      appDir,
+      settings,
+      meteorPath,
+      port = 3000,
+    } = options || {};
 
-  if (!appDir) throw new Error('appDir not specified!');
+    if (!appDir) throw new Error('appDir not specified!');
 
-  const meteorApp = new MeteorApp({
-    args,
-    appDir,
-    port,
-    settings,
-    meteorPath,
-  });
-
-  return Promise.all([
-    meteorApp.launch(),
-    startPuppeteer(puppeteerOptions),
-  ]).then(([meteor, puppeteer]) => {
-    puppeteer.meteor = meteorApp; // eslint-disable-line no-param-reassign
-    puppeteer.on('close', () => {
-      meteor.kill();
+    this.meteorApp = new MeteorApp({
+      args,
+      appDir,
+      port,
+      settings,
+      meteorPath,
     });
 
-    return puppeteer.goto(`http://localhost:${port}`).then(() => puppeteer);
-  });
+    this.meteorApp.on('stdout', data => this.emit('stdout', data.toString()));
+    this.meteorApp.on('stderr', data => this.emit('stderr', data.toString()));
+  }
+
+  start() {
+    return Promise.all([
+      this.meteorApp.launch(),
+      startPuppeteer(this.options.puppeteerOptions),
+    ]).then(([meteor, puppeteer]) => {
+      puppeteer.meteor = this.meteorApp; // eslint-disable-line no-param-reassign
+      puppeteer.on('close', () => {
+        meteor.kill();
+      });
+
+      return puppeteer.goto(`http://localhost:${this.options.port || 3000}`).then(() => puppeteer);
+    });
+  }
+}
+
+export default (options) => {
+  const show = new Spaceshow(options);
+  return show.start();
 };
